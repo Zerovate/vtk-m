ea3553128f2b73f7d711bc0ce934788aeea44812
Li-Ta Lo <ollie@lanl.gov>
2020-06-24 15:47:28 -0600

    Add Copyright notice

--- examples/heat_diffusion/hd_worklets.h
+++ examples/heat_diffusion/hd_worklets.h
@@ -1,3 +1,13 @@
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//============================================================================
+
 #ifndef HD_WORKLET_H
 #define HD_WORKLET_H
 #include <vtkm/cont/ArrayHandle.h>
--- examples/heat_diffusion/heat_diffusion.cxx
+++ examples/heat_diffusion/heat_diffusion.cxx
@@ -1,3 +1,12 @@
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//============================================================================
 //This work was supported by Commissariat a l'Energie Atomique    * CEA, DAM, DIF, F-91297 Arpajon, France.
 
 #ifdef ANIMATE
