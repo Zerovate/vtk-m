e82282b48f9c61735bcb62880994064f474b4a86
Kenneth Moreland <morelandkd@ornl.gov>
2021-11-08 10:56:29 -0700

    WIP: Trying to narrow down crash on MSVC

--- vtkm/rendering/raytracing/RayOperations.cxx
+++ vtkm/rendering/raytracing/RayOperations.cxx
@@ -21,14 +21,23 @@ void RayOperations::MapCanvasToRays(Ray<vtkm::Float32>& rays,
 {
   vtkm::Id width = canvas.GetWidth();
   vtkm::Id height = canvas.GetHeight();
+  std::cout << "        MatrixMultiply" << std::endl;
   vtkm::Matrix<vtkm::Float32, 4, 4> projview =
     vtkm::MatrixMultiply(camera.CreateProjectionMatrix(width, height), camera.CreateViewMatrix());
   bool valid;
+  std::cout << "        MatrixInverse" << std::endl;
   vtkm::Matrix<vtkm::Float32, 4, 4> inverse = vtkm::MatrixInverse(projview, valid);
+  std::cout << "        valid: " << valid << std::endl;
   (void)valid; // this can be a false negative for really tiny spatial domains.
+  vtkm::cont::printSummary_ArrayHandle(rays.PixelIdx, std::cout);
+  vtkm::cont::printSummary_ArrayHandle(rays.MaxDistance, std::cout);
+  vtkm::cont::printSummary_ArrayHandle(rays.Origin, std::cout);
+  vtkm::cont::printSummary_ArrayHandle(canvas.GetDepthBuffer(), std::cout);
+  std::cout << "        RayMapCanvas" << std::endl;
   vtkm::worklet::DispatcherMapField<detail::RayMapCanvas>(
     detail::RayMapCanvas(inverse, width, height, camera.GetPosition()))
     .Invoke(rays.PixelIdx, rays.MaxDistance, rays.Origin, canvas.GetDepthBuffer());
+  std::cout << "        MapCanvasToRays complete" << std::endl;
 }
 }
 }
