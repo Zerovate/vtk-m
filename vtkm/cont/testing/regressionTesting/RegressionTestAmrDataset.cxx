6da9589732295546ef73bc01f11dffb83c512b7c
Roxana Bujack <bujack@lanl.gov>
2021-11-16 16:49:12 -0700

    use just partition 0 without blanks

--- vtkm/cont/testing/regressionTesting/RegressionTestAmrDataset.cxx
+++ vtkm/cont/testing/regressionTesting/RegressionTestAmrDataset.cxx
@@ -41,14 +41,14 @@ void TestAmrDatasetExecute(int dim, int numberOfLevels, int cellsPerDimension)
   std::cout << "Hier amr " << std::endl;
   amrDataSet.PrintSummary(std::cout);
 
-  //  // Remove blanked cells
-  //  vtkm::filter::Threshold threshold;
-  //  threshold.SetLowerThreshold(0);
-  //  threshold.SetUpperThreshold(1);
-  //  threshold.SetActiveField("vtkGhostType");
-  //  vtkm::cont::PartitionedDataSet derivedDataSet = threshold.Execute(amrDataSet);
-  //  std::cout << "Hier derived " << std::endl;
-  //  derivedDataSet.PrintSummary(std::cout);
+  // Remove blanked cells
+  vtkm::filter::Threshold threshold;
+  threshold.SetLowerThreshold(0);
+  threshold.SetUpperThreshold(1);
+  threshold.SetActiveField("vtkGhostType");
+  vtkm::cont::PartitionedDataSet derivedDataSet = threshold.Execute(amrDataSet);
+  std::cout << "Hier derived " << std::endl;
+  derivedDataSet.PrintSummary(std::cout);
 
   //  // Extract surface for efficient 3D pipeline
   //  vtkm::filter::ExternalFaces surface;
@@ -62,7 +62,7 @@ void TestAmrDatasetExecute(int dim, int numberOfLevels, int cellsPerDimension)
   //  std::cout << "Hier merged " << std::endl;
   //  result.PrintSummary(std::cout);
 
-  vtkm::rendering::testing::RenderAndRegressionTest<M, C, V3>(amrDataSet.GetPartition(0),
+  vtkm::rendering::testing::RenderAndRegressionTest<M, C, V3>(derivedDataSet.GetPartition(0),
                                                               "RTDataCells",
                                                               vtkm::cont::ColorTable("inferno"),
                                                               "cont/amr" + std::to_string(dim) +
