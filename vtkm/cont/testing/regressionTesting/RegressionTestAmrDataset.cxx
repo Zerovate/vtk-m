8c1315554c1adbbab17241361ff833e942cfdbb8
Roxana Bujack <bujack@lanl.gov>
2021-11-16 16:35:02 -0700

    use just partition 0

--- vtkm/cont/testing/regressionTesting/RegressionTestAmrDataset.cxx
+++ vtkm/cont/testing/regressionTesting/RegressionTestAmrDataset.cxx
@@ -31,38 +31,6 @@ void TestAmrDatasetExecute(int dim, int numberOfLevels, int cellsPerDimension)
 {
   std::cout << "Generate Image for AMR" << std::endl;
 
-  char buffer[PATH_MAX];
-  if (getcwd(buffer, sizeof(buffer)) != NULL)
-  {
-    printf("Current working directory : %s\n", buffer);
-  }
-
-  using vtkm::cont::testing::Testing;
-  std::string baselinePath = Testing::RegressionImagePath("cont");
-  std::cout << "Hier list of files in baseline path " << baselinePath << std::endl;
-  DIR* dir;
-  struct dirent* ent;
-  if ((dir = opendir(baselinePath.c_str())) != NULL)
-  {
-    /* print all the files and directories within directory */
-    while ((ent = readdir(dir)) != NULL)
-    {
-      printf("%s\n", ent->d_name);
-    }
-    closedir(dir);
-  }
-
-  std::cout << "Hier list of files in ./../../../../../build/cont/" << std::endl;
-  if ((dir = opendir("./../../../../../build/cont/")) != NULL)
-  {
-    /* print all the files and directories within directory */
-    while ((ent = readdir(dir)) != NULL)
-    {
-      printf("%s\n", ent->d_name);
-    }
-    closedir(dir);
-  }
-
   using M = vtkm::rendering::MapperRayTracer;
   using C = vtkm::rendering::CanvasRayTracer;
   using V3 = vtkm::rendering::View3D;
@@ -73,46 +41,28 @@ void TestAmrDatasetExecute(int dim, int numberOfLevels, int cellsPerDimension)
   std::cout << "Hier amr " << std::endl;
   amrDataSet.PrintSummary(std::cout);
 
-  // Remove blanked cells
-  vtkm::filter::Threshold threshold;
-  threshold.SetLowerThreshold(0);
-  threshold.SetUpperThreshold(1);
-  threshold.SetActiveField("vtkGhostType");
-  vtkm::cont::PartitionedDataSet derivedDataSet = threshold.Execute(amrDataSet);
-  std::cout << "Hier derived " << std::endl;
-  derivedDataSet.PrintSummary(std::cout);
+  //  // Remove blanked cells
+  //  vtkm::filter::Threshold threshold;
+  //  threshold.SetLowerThreshold(0);
+  //  threshold.SetUpperThreshold(1);
+  //  threshold.SetActiveField("vtkGhostType");
+  //  vtkm::cont::PartitionedDataSet derivedDataSet = threshold.Execute(amrDataSet);
+  //  std::cout << "Hier derived " << std::endl;
+  //  derivedDataSet.PrintSummary(std::cout);
 
-  // Extract surface for efficient 3D pipeline
+  //  // Extract surface for efficient 3D pipeline
   //  vtkm::filter::ExternalFaces surface;
   //  //  surface.SetPassPolyData(1);
   //  //  surface.SetFieldsToPass("RTDataCells");
   //  derivedDataSet = surface.Execute(derivedDataSet);
 
-  // Merge dataset
-  vtkm::cont::DataSet result = vtkm::cont::MergePartitionedDataSet(derivedDataSet);
-  result.PrintSummary(std::cout);
-  std::cout << "Hier merged " << std::endl;
-  result.PrintSummary(std::cout);
+  //  // Merge dataset
+  //  vtkm::cont::DataSet result = vtkm::cont::MergePartitionedDataSet(derivedDataSet);
+  //  result.PrintSummary(std::cout);
+  //  std::cout << "Hier merged " << std::endl;
+  //  result.PrintSummary(std::cout);
 
-  std::ifstream in1(baselinePath + "/amr2D.png", std::ifstream::ate | std::ifstream::binary);
-  std::cout << "Hier baseline " << in1.tellg() << std::endl;
-  std::ifstream in2("./../../../../../build/cont/test-amr2D.png",
-                    std::ifstream::ate | std::ifstream::binary);
-  std::cout << "Hier temp " << in2.tellg() << std::endl;
-  std::ifstream in3("./../../../../../build/cont/diff-amr2D.png",
-                    std::ifstream::ate | std::ifstream::binary);
-  std::cout << "Hier diff " << in3.tellg() << std::endl;
-
-  std::ifstream in4(baselinePath + "/amr3D.png", std::ifstream::ate | std::ifstream::binary);
-  std::cout << "Hier baseline " << in4.tellg() << std::endl;
-  std::ifstream in5("./../../../../../build/cont/test-amr3D.png",
-                    std::ifstream::ate | std::ifstream::binary);
-  std::cout << "Hier temp " << in5.tellg() << std::endl;
-  std::ifstream in6("./../../../../../build/cont/diff-amr3D.png",
-                    std::ifstream::ate | std::ifstream::binary);
-  std::cout << "Hier diff " << in6.tellg() << std::endl;
-
-  vtkm::rendering::testing::RenderAndRegressionTest<M, C, V3>(result,
+  vtkm::rendering::testing::RenderAndRegressionTest<M, C, V3>(amrDataSet.GetPartition(0),
                                                               "RTDataCells",
                                                               vtkm::cont::ColorTable("inferno"),
                                                               "cont/amr" + std::to_string(dim) +
