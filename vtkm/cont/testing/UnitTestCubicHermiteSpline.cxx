f84ea4a64948526262aeb51d651a786dc02534d4
David Pugmire <dpugmire@gmail.com>
2025-04-07 07:58:56 -0400

    Increase the coverage of the unit tests.

--- vtkm/cont/testing/UnitTestCubicHermiteSpline.cxx
+++ vtkm/cont/testing/UnitTestCubicHermiteSpline.cxx
@@ -61,8 +61,8 @@ void CheckEvaluation(const vtkm::cont::CubicHermiteSpline& spline,
 
 void CubicHermiteSplineTest()
 {
-  std::vector<vtkm::Vec3f> pts = { { 0, 0, 0 },  { 1, 1, 0 },  { 2, 1, 0 }, { 3, -.5, 0 },
-                                   { 4, -1, 0 }, { 5, -1, 0 }, { 6, 0, 0 } };
+  std::vector<vtkm::Vec3f> pts = { { 0, 0, 0 },  { 1, 1, 1 },  { 2, 1, 0 }, { 3, -.5, -1 },
+                                   { 4, -1, 0 }, { 5, -1, 1 }, { 6, 0, 0 } };
 
   vtkm::cont::CubicHermiteSpline spline(pts);
   //Evaluation at knots gives the sample pts.
@@ -70,10 +70,10 @@ void CubicHermiteSplineTest()
 
   //Evaluation at non-knot values.
   std::vector<vtkm::FloatDefault> params = { 0.21, 0.465, 0.501, 0.99832 };
-  std::vector<vtkm::Vec3f> result = { { 1.20072, 1.07379, 0 },
-                                      { 2.64681, 0.012108, 0 },
-                                      { 2.79356, -0.239254, 0 },
-                                      { 5.99081, -0.00924034, 0 } };
+  std::vector<vtkm::Vec3f> result = { { 1.23261, 1.08861, 0.891725 },
+                                      { 2.68524, -0.0560059, -0.855685 },
+                                      { 2.85574, -0.32766, -0.970523 },
+                                      { 5.99045, -0.00959875, 0.00964856 } };
   CheckEvaluation(spline, params, result);
 
   //Explicitly set knots and check.
@@ -81,17 +81,24 @@ void CubicHermiteSplineTest()
   spline = vtkm::cont::CubicHermiteSpline(pts, knots);
   CheckEvaluation(spline, knots, pts);
 
+  //Evaluation at non-knot values.
+  params = { 0.84, 1.399, 2.838, 4.930, 5.001, 5.993 };
+  result = { { 0.84, 0.896448, 0.952896 },    { 1.399, 1.14382, 0.745119 },
+             { 2.838, -0.297388, -0.951764 }, { 4.93, -1.03141, 0.990543 },
+             { 5.001, -0.999499, 0.999998 },  { 5.993, -0.00702441, 0.00704873 } };
+  CheckEvaluation(spline, params, result);
+
   //Non-uniform knots.
   knots = { 0, 1, 2, 2.1, 2.2, 2.3, 3 };
   spline = vtkm::cont::CubicHermiteSpline(pts, knots);
   CheckEvaluation(spline, knots, pts);
 
   params = { 1.5, 2.05, 2.11, 2.299, 2.8 };
-  result = { { 1.39773, 1.23295, 0 },
-             { 2.39773, 0.357954, 0 },
-             { 3.1, -0.59275, 0 },
-             { 4.99735, -1.00125, 0 },
-             { 5.75802, -0.293003, 0 } };
+  result = { { 1.39773, 1.23295, 0.727273 },
+             { 2.39773, 0.357954, -0.522727 },
+             { 3.1, -0.59275, -0.981 },
+             { 4.99735, -1.00125, 0.999801 },
+             { 5.75802, -0.293003, 0.344023 } };
   CheckEvaluation(spline, params, result);
 
   //Create a more complex spline from analytical functions.
@@ -104,7 +111,7 @@ void CubicHermiteSplineTest()
   {
     vtkm::FloatDefault x = vtkm::Cos(t);
     vtkm::FloatDefault y = vtkm::Sin(t);
-    vtkm::FloatDefault z = vtkm::Cos(t) * vtkm::Sin(t);
+    vtkm::FloatDefault z = x * y;
     pts.push_back({ x, y, z });
     knots.push_back(t);
     t += dt;
@@ -112,7 +119,8 @@ void CubicHermiteSplineTest()
   spline = vtkm::cont::CubicHermiteSpline(pts, knots);
   CheckEvaluation(spline, knots, pts);
 
-  params = { 0.15, 2.38, 4.92, 6.2 };
+  //Evaluate at a few points and check against analytical results.
+  params = { 0.15, 1.83, 2.38, 3.0291, 3.8829, 4.92, 6.2 };
   result.clear();
   for (const auto& p : params)
     result.push_back({ vtkm::Cos(p), vtkm::Sin(p), vtkm::Cos(p) * vtkm::Sin(p) });
